d9c5c5ab04822d01fd0270af932df5d76f98ad19
Radhey Shah <radhey.shah@microsoft.com>
2020-07-28 23:21:15 -0700

    Updated the ADLS location to jbilcdm, and updated CDM files with model.json and fixed files which didn't have dateTime in them.

--- Model/apps/miabdtdl2cdmconverter/CDMExperiments/Program.cs
+++ Model/apps/miabdtdl2cdmconverter/CDMExperiments/Program.cs
@@ -29,7 +29,7 @@ namespace MiaB.model.dtdl2cdm
         private const string adls = "adls";
         private const string adlsHostName = "gen2hackstore.dfs.core.windows.net";
         // Remember ADLS allows only lowercase letters, numbers and hyphens.This root path should be available in ADLS - i.e. needs to be created manually
-        private const string adlsRoot = "/cdm/hack2020";
+        private const string adlsRoot = "/cdm/jabilcdm";
         // SECURITY RISK - this should be saved in AKV and retrived from there. Hardcodign for quick hacking
         private const string adlsAccessKey = "xnv4EoFbGh53d5n2669F5CniZYRnY/EfDQSz6vStu22m4m/pJlq9zn0nfI8UsQvvtixM/kIoxC4xpinHSYV7ZQ==";
         
